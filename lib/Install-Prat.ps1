0af2d0f1619984accca5c8d8f7ef82f1649c51ed
Andrew Godfrey <andrew@gfreys.com>
2024-05-16 12:00:45 -0700

    Install-Prat: Oops, revert last change to 'git clone' error handling

--- lib/Install-Prat.ps1
+++ lib/Install-Prat.ps1
@@ -36,7 +36,7 @@ $env:path += ";$gitDir" # The git package adds itself to PATH in the registry, b
 $target = $home + "\prat"
 $source = "https://github.com/AndrewGodfrey/prat.git"
 if (!(Test-Path $target)) { git clone $source $target }
-if ((!$?) -or !(Test-Path $target)) { throw "'git clone prat' failed" }
+if (($lastExitCode -ne 0) -or !(Test-Path $target)) { throw "'git clone prat' failed" }
 
 pushd $target
 try {
