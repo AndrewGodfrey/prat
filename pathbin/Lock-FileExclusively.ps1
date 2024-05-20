1c311cdd9d518d8cc23fad3af961e4a7f6a56214
Andrew Godfrey <andrew@gfreys.com>
2024-05-20 09:12:57 -0700

    Add test tool 'Lock-FileExclusively'

---
+++ pathbin/Lock-FileExclusively.ps1
@@ -0,0 +1,20 @@
+# .SYNOPSIS
+# Test tool for grabbing an exclusive lock on a file
+
+# Source: https://superuser.com/questions/857725/how-to-lock-a-file-in-windows-without-changing-it-or-using-third-party-tools/857735#857735
+
+param($FileName)
+$ErrorActionPreference = "stop"
+
+$FileName = Resolve-Path $FileName
+
+#Open the file in read only mode, without sharing (I.e., locked as requested)
+$file = [System.io.File]::Open($FileName, 'Open', 'Read', 'None')
+
+#Wait in the above (file locked) state until the user presses a key
+Write-Host "Press any key to continue ..."
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+#Close the file (This releases the current handle and unlocks the file)
+$file.Close()
+
