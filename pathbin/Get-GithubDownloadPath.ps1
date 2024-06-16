8ad2228935a6c314be2cea36518f7926a5319600
Andrew Godfrey <andrew@gfreys.com>
2024-06-16 15:22:13 -0700

    Add script 'Get-GithubDownloadPath'

--- pathbin/Get-GithubDownloadPath.ps1
+++ pathbin/Get-GithubDownloadPath.ps1
@@ -1,3 +1,11 @@
+# Given a file, finds its direct 'raw' url on github.
+# Could be extended to other online repos that support raw paths (not all do).
+#
+# Don't overuse this. Its purpose is 'bootstrapping'. I can think of two kinds:
+# 1. Prat installation currently uses a raw url to get Install-Prat.ps1.
+# 2. While this is fragile: If you want to point someone at a standalone script in a repo without
+#    them having to install the whole repo. It's fragile because the script could later be updated to depend on
+#    other things in the repo. But it makes sense for tiny scripts - say Get-ErrorStatuses.ps1, or Lock-FileExclusively.ps1.
 param ($file)
 if (!(Test-Path $file)) { throw "Not found: $file" }
 
