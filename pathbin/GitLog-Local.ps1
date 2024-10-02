01b9b4f923230da9767832ff8f102755680f506d
Andrew Godfrey <andrew@gfreys.com>
2024-10-01 22:17:15 -0700

    gll: Change its default to show the last 7 items regardless of age. Also allow focusing on one file
    With overrides to either change n, or specify # of months

--- pathbin/GitLog-Local.ps1
+++ pathbin/GitLog-Local.ps1
@@ -1,6 +1,17 @@
 # .SYNOPSIS
 # Shows git history under the current directory, for the last n months (default 3).
 # Uses a concise one-line format, that still includes date and author information.
-param ($Months=3)
-git log --after="$Months months ago" --pretty="%C(auto)%as: %<(18,trunc)%an %h  %Cgreen%s%Creset" .
+param ($Path=".", $n=$null, $Months=$null)
+
+$optArgs = @()
+if ($null -ne $Months) {
+    if ($null -ne $n) {
+        throw "Cannot specify both Months and n"
+    }
+    $optArgs += "--after=""$Months months ago""" 
+} else {
+    if ($null -eq $n) { $n = 7 }
+    $optArgs += "--max-count=$n"
+}
+git log @optArgs --pretty="%C(auto)%as: %<(18,trunc)%an %h  %Cgreen%s%Creset" $Path
 
