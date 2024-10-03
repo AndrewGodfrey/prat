d6cfc19178e2c2228084d650f0f95ed0bc73e68a
Andrew Godfrey <andrew@gfreys.com>
2024-10-02 18:37:16 -0700

    ow: Add a 'file' parameter that lets you open a particular file

--- pathbin/Open-CodebaseWorkspace.ps1
+++ pathbin/Open-CodebaseWorkspace.ps1
@@ -1,22 +1,28 @@
-# Opens the workspace for the current codebase / sub-codebase
+# Opens the given workspace, or (if null) the workspace for the current codebase / sub-codebase
+# A 'workspace' can be something like a Visual Studio .sln file, a Vscode .code-workspace, or any
+# other workspace file that Shell can open.
 [CmdletBinding()]
-param()
+param($file = $null)
 
 $cbt = &$PSScriptRoot\..\lib\Get-CodebaseSubTable $pwd -Verbose:$VerbosePreference
 Write-DebugValue $cbt
 
-if ($null -eq $cbt) { 
+if (($null -eq $cbt) -and ($null -eq $file)) { 
     Write-Error "Codebase not recognized"
     return
 }
 
-$workspace = $cbt.workspace
-if ($null -eq $workspace) {
-    throw "Don't know how to open workspace for '$($cbt.id)'"
+if ($null -eq $file) {
+    $workspace = $cbt.workspace
+    if ($null -eq $workspace) {
+        throw "Don't know how to open workspace for '$($cbt.id)'"
+    }
+    $workspace = $workspace -replace "dev:", "$($cbt.root)/"
+    if ($workspace.Contains("test:")) { throw "NYI" }
+} else {
+    $workspace = $file
 }
-$workspace = $workspace -replace "dev:", "$($cbt.root)/"
-if ($workspace.Contains("test:")) { throw "NYI" }
-if (!(Test-Path $workspace)) { throw "Not found: $workspace" }
 
+if (!(Test-Path $workspace)) { throw "Not found: $workspace" }
 Write-Verbose "Opening workspace: $workspace"
 Invoke-CommandWithCachedEnvDelta {&$workspace} $cbt.cachedEnvDelta
