624dbb58aadaca9aedee3c2808e4515be51dfaa3
Andrew Godfrey <andrew@gfreys.com>
2024-10-10 09:13:48 -0700

    ConvertTo-Expression: Add some basic tests
    Including a commented-out one for scriptblocks

---
+++ lib/PratBase/ConvertTo-Expression.tests.ps1
@@ -0,0 +1,66 @@
+BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1','.ps1')
+
+    function AssertRoundtrip($expression, $convertedExpression, [switch] $ValueType = $false) {
+        $newExpression = Invoke-Expression $convertedExpression
+
+        # Assert
+        $newExpression.GetType().Name | Should -Be $expression.GetType().Name
+        if ($ValueType) {
+            $expression -eq $newExpression | Should -BeTrue
+        }
+
+        # Optionally return the expression, in case caller wants to do more validation
+        return $newExpression
+    }
+}
+
+Describe "ConvertTo-Expression" {
+    It "converts a basic integer" {
+        $e = 42
+
+        # Act
+        $result = ConvertTo-Expression $e
+
+        # Assert
+        AssertRoundtrip $e $result -ValueType
+        $result | Should -Be "42"
+    }
+
+    It "converts a basic string" {
+        $e = "foo"
+
+        # Act
+        $result = ConvertTo-Expression $e
+
+        # Assert
+        AssertRoundtrip $e $result -ValueType
+    }
+
+    It "converts a hashtable" {
+        $e = @{ foo = 1; bar = 42 }
+
+        # Act
+        $result = ConvertTo-Expression $e
+
+        # Assert
+        $resultExpression = AssertRoundtrip $e $result
+
+        $resultExpression.Count | Should -Be 2
+        $resultExpression.foo | Should -Be 1
+        $resultExpression.bar | Should -Be 42
+    }
+
+    # Can't run this, because it hangs
+    # It "chokes when given a scriptblock" {
+    #     $e = { echo 43 }
+
+    #     # Act
+    #     $result = ConvertTo-Expression $e
+
+    #     # Assert
+    #     $resultExpression = AssertRoundtrip $e $result
+    #     &$resultExpression | Should -Be "43"
+    # }
+}
+
