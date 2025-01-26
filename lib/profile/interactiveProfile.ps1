0ba9bf5bfb5252fec9b768dd96f62c029d468322
Andrew Godfrey <andrew@gfreys.com>
2025-01-25 22:40:31 -0800

    Add 'Get-LatestFiles.ps1' (alias: lsl)

--- lib/profile/interactiveProfile.ps1
+++ lib/profile/interactiveProfile.ps1
@@ -90,6 +90,7 @@ New-Alias -Name ow -Value Open-CodebaseWorkspace -Scope Global
 New-Alias -Name gtfe -Value Get-TextFileEncoding -Scope Global
 New-Alias -Name gll -Value GitLog-Local -Scope Global
 New-Alias -Name e -Value Open-FileInEditor -Scope Global
+New-Alias -Name lsl -Value Get-LatestFiles -Scope Global
 New-Alias -Name ude -Value Update-DevEnvironment -Scope Global
 
 pratProfile_trace "End:   interactiveProfile.ps1"
---
+++ pathbin/Get-LatestFiles.ps1
@@ -0,0 +1,16 @@
+# .SYNOPSIS
+# Get-LatestFiles (alias: lsl) - get the most recent n files
+param($filespec=$null, $n, [switch] $Recurse=$false)
+$default_n = 10
+
+if ($null -eq $n) { $n = $default_n }
+
+function Get-RelativePath($path) {
+    $p = $pwd.Path
+    if ($path.StartsWith($p)) { "." + $path.Substring($p.Length) } else { $path }
+}
+
+Get-ChildItem $filespec -Recurse:$Recurse | 
+    Sort-Object -Property LastWriteTime -Descending | 
+    Select-Object -First $n |
+    Format-Table -Property Mode, LastWriteTime, @{Name="Length";Expression={Get-OptimalSize $_.Length}}, @{Name="Path";Expression={Get-RelativePath $_.FullName}}
