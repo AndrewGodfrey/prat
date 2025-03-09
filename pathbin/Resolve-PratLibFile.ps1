79accef61cf6173998d7cbf59cfbba08809b8ed0
Andrew Godfrey <andrew@gfreys.com>
2025-03-09 10:41:27 -0700

    Resolve-PratLibFile: Add 'ListAll' param

--- pathbin/Resolve-PratLibFile.ps1
+++ pathbin/Resolve-PratLibFile.ps1
@@ -4,15 +4,27 @@
 #      another dev environment overrides it. Note the "_prat" suffix. Appending the dev environment name serves 2 purposes:
 #      1. The files are easier to distinguish in an IDE.
 #      2. It makes clear which files are expected to be overridden.
-param ($file)
+#
+# .PARAMETER ListAll
+# Instead of returning the first matching filename, returns an array of all matching filenames.
+param ($file, [switch] $ListAll)
 
 $ext = Split-Path $file -Extension
 $fileWithoutExt = $file.SubString(0, $file.Length - $ext.Length)
 
+$listAllResults = @()
+
 foreach ($de in (Get-DevEnvironments)) {
     $candidate = "$($de.Path)\$($fileWithoutExt)_$($de.Name)$ext"
     if (Test-Path $candidate) {
+        if ($ListAll) {
+            $listAllResults += $candidate
+            continue
+        }
         return $candidate
     }
+}
+if ($ListAll) {
+    return $listAllResults
 }
 return $null
