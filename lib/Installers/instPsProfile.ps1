d20b6f1c2e43a32d4395508e666a700df799c51f
Andrew Godfrey <andrew@gfreys.com>
2025-03-09 16:48:24 -0700

    Install-PsProfile: Handle OneDrive messing things up

--- lib/Installers/instPsProfile.ps1
+++ lib/Installers/instPsProfile.ps1
@@ -1,28 +1,35 @@
 # Installs Powershell profile, for both Powershell, and for Windows Powershell [the older version of Powershell that is included in Windows].
+function installOne($targetDir, $targetFile) {
+    if (!(Test-Path $targetDir)) {
+        Write-Host "Create dir: $targetDir"
+        New-Item -Type Directory $targetDir | Out-Null
+    }
+
+    # Save a copy of the 'original' file we found before Prat first ran.
+    $backupFile = "$targetDir\profile.original.prat.ps1"
+    
+    if (!(Test-Path $backupFile)) {
+        if (Test-Path "$targetDir\$targetFile") {
+            copy $targetDir\$targetFile -Destination $backupFile
+        } else {
+            Set-Content $backupFile "# Install-PsProfile found no profile.ps1"
+        }
+    }
+
+    Install-File $stage $PSScriptRoot $targetDir "installedProfile.ps1" $targetFile
+}
+
 function Install-PsProfile($installationTracker) {
     $stage = $installationTracker.StartStage("profile.ps1")
 
-    @('Powershell', 'WindowsPowerShell') | % {
-        $targetDir = "$home\Documents\$_"
-        if (!(Test-Path $targetDir)) {
-            Write-Host "Create dir: $targetDir"
-            New-Item -Type Directory $targetDir | Out-Null
-        }
-        $targetFile = "profile.ps1"
+    # OneDrive may have moved the profile, sigh.
+    $targetDir = Split-Path -Parent $profile.CurrentUserAllHosts
+    $targetFile = Split-Path -Leaf $profile.CurrentUserAllHosts
+    installOne $targetDir $targetFile
 
-        # Save a copy of the 'original' file we found before Prat first ran.
-        $backupFile = "$targetDir\profile.original.prat.ps1"
-        
-        if (!(Test-Path $backupFile)) {
-            if (Test-Path "$targetDir\$targetFile") {
-                copy $targetDir\$targetFile -Destination $backupFile
-            } else {
-                Set-Content $backupFile "# Install-PsProfile found no profile.ps1"
-            }
-        }
+    # We'll just guess that the WindowsPowerShell profile is nearby, and has a default name.
+    installOne "$targetDir\..\WindowsPowerShell" "profile.ps1"
 
-        Install-File $stage $PSScriptRoot $targetDir "installedProfile.ps1" $targetFile
-    }
     $installationTracker.EndStage($stage)
 }
 
