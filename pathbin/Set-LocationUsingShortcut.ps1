d523089937afb2f3a56b2e796823f7390c8b5b39
Andrew Godfrey <andrew@gfreys.com>
2025-03-09 10:28:16 -0700

    c: Replace implementation for '?'

--- pathbin/Set-LocationUsingShortcut.ps1
+++ pathbin/Set-LocationUsingShortcut.ps1
@@ -7,13 +7,6 @@
 # Shortcuts are interpreted using Find-Shortcut and Find-CodebaseShortcut
 param($Shortcut="", [switch] $Test, [switch] $ListAll)
 
-function emit($key, $target) {
-    $target = $target -replace "\\", "/"
-    $p = 20-$key.Length
-    if ($p -lt 1) { $p = 1 }
-    $padding = ' ' * $p
-    echo "$key$padding$target"
-}
 
 function GetAllShortcuts() {
     [System.Collections.Specialized.OrderedDictionary] $result = &$PSScriptRoot/../lib/Find-Shortcut -ListAll
@@ -36,21 +29,7 @@ if ($ListAll) {
 }
 
 if ($Shortcut -eq "?") {
-    $globalShortcuts = &$PSScriptRoot/../lib/Find-Shortcut -ListAll
-
-    foreach ($key in ($globalShortcuts.Keys | Sort-Object)) {
-        emit $key $globalShortcuts[$key]
-    }
-
-    $cbts = &$PSScriptRoot/../lib/Find-CodebaseShortcut -ListAll
-    foreach ($cbt in $cbts) {
-        # echo "$($cbt.id):"
-        foreach ($key in ($cbt.shortcuts.Keys | Sort-Object)) {
-            $target = $cbt.root + "/" + $cbt.shortcuts[$key]
-            emit $key $target
-        }
-    }
-    return
+    return (GetAllShortcuts | Format-Table -HideTableHeaders)
 }
 
 function findTestDir($tt) {
