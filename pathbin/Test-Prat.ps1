7f4160bc9bdd48a4b6eec564c43627c127bffe02
Andrew Godfrey <andrew@gfreys.com>
2025-05-18 20:59:57 -0700

    Test-Prat: Make coverage relative to the subdirectory we're focused on

--- pathbin/Test-Prat.ps1
+++ pathbin/Test-Prat.ps1
@@ -2,12 +2,13 @@ param ([switch] $CodeCoverage)
 
 Import-Module Pester
 $Configuration = [PesterConfiguration]::Default
+$Configuration.Run.Path = "."
 
 if ($CodeCoverage) {
     $pratRoot = Resolve-Path "$PSScriptRoot\.."
     $Configuration.CodeCoverage.Enabled = [bool] $true
     $Configuration.CodeCoverage.OutputFormat = "CoverageGutters"
-    $Configuration.CodeCoverage.Path = $pratRoot
+    $Configuration.CodeCoverage.Path = $Configuration.Run.Path
     $Configuration.CodeCoverage.OutputPath = "$pratRoot/auto/coverage.xml"
     $Configuration.CodeCoverage.CoveragePercentTarget = 70
 }
