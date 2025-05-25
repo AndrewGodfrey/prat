dcd60bfa49d174dfd026e8351f8a175dc59b4974
Andrew Godfrey <andrew@gfreys.com>
2025-05-25 15:28:10 -0700

    Add test for Convert-BookmarkToLnks.ps1

--- pathbin/Convert-BookmarkToLnks.ps1
+++ pathbin/Convert-BookmarkToLnks.ps1
@@ -1,11 +1,24 @@
 # Used for moving links to hp.lnks. 
 # Copy a link to the clipboard (e.g. from Firefox bookmarks), and then run this.
 
-$aobj = Get-Clipboard -TextFormatType Html
-$a = [System.String]::Join("`n", $aobj)
-if ($a -match '<!--StartFragment--><A HREF="(?<link>.+)">(?<text>.+)</A>') {
-   $result = ("`t[" + $Matches.text + "](" + $Matches.link + ")`r`n")
-   Set-Clipboard $result
-   Write-Host "Success"
+if ($PSVersiontable.PSEdition -eq "Core") {
+   function getHtmlFromClipboard {
+      Add-Type -AssemblyName System.Windows.Forms
+      [System.Windows.Forms.Clipboard]::GetData("HTML Format")
+   }
+} else {
+   function getHtmlFromClipboard {
+      Get-Clipboard -TextFormatType Html
+   }
+}
+
+$aobj = getHtmlFromClipboard
+if ($null -ne $aobj) {
+   $a = [System.String]::Join("`n", $aobj)
+   if ($a -match '<!--StartFragment--><A HREF="(?<link>.+)">(?<text>.+)</A>') {
+      $result = ("`t[" + $Matches.text + "](" + $Matches.link + ")`r`n")
+      Set-Clipboard $result
+      Write-Host "Success"
+   }
 }
 
