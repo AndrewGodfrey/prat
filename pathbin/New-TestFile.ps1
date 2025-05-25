2c45f0e0aa2fb8452b5bbe4f49d4f37ef5abd921
Andrew Godfrey <andrew@gfreys.com>
2025-05-25 15:30:51 -0700

    New-TestFile: Support pathbin files

--- pathbin/New-TestFile.ps1
+++ pathbin/New-TestFile.ps1
@@ -5,44 +5,70 @@ if (!(Test-Path $file)) {
     throw "Not found: $file"
 }
 
-if ($file.EndsWith(".ps1")) {
-    $testFile = $file -replace "\.ps1$", ".Tests.ps1"
-    if (Test-Path $testFile) {
-        throw "Test file already exists: $testFile"
-    }
-
-    $functions = Get-Content $file | 
-        Where-Object { $_ -match "function\s" } |
-        ForEach-Object { $_ -replace '^\s*function\s+([^\s\(]+).*$', '$1' }
-
-    if ($functions.Count -eq 0) {
-        throw "No functions found in $file"
-    }
-
-    # TODO: Detect scripts aren't dot-sourceable. Ones which define functions will get past
-    #       the check above. They can be made dot-sourceable by adding:
-    #           if ($MyInvocation.InvocationName -ne ".") {
-
-    Write-Debug "Functions found: $($functions -join ", ")"
-
-    $testContent = @"
+function GetTestContent($functions) {
+    @"
 BeforeAll {
     . `$PSCommandPath.Replace('.Tests.ps1','.ps1')
 }
 
 
 "@
-
     $first = $true
     foreach ($function in $functions) {
-        if ($first) { $first = $false } else { $testContent += "`n" }
-        $testContent += 'Describe "' + $function + '" {' + "`n"
-        $testContent += @"
+        if ($first) { $first = $false } else { "`n" }
+        'Describe "' + $function + '" {' + "`n"
+        @"
     It "" {
     }
 }
 
 "@
+    }
+}
+
+function GetPathbinTestContent($command) {
+    @"
+Describe "$command" {
+    It "" {
+        $command 
+    }
+}
+
+"@
+}
+
+function IsPathbinFile($file) {
+    return (Resolve-Path $file) -match '\\prat\\pathbin\\[^\\/]+$'
+}
+
+function GetTestFile($file) {
+    if (IsPathbinFile $file) {
+        $file = Join-Path (Split-Path $file) "tests" (Split-Path -Leaf $file)
+    }
+    return $file -replace "\.ps1$", ".Tests.ps1"
+}
+
+if ($file.EndsWith(".ps1")) {
+    $testFile = GetTestFile $file
+    if (Test-Path $testFile) {
+        throw "Test file already exists: $testFile"
+    }
+    if (IsPathbinFile $file) {
+        $command = Split-Path -LeafBase $file
+        $testContent = GetPathbinTestContent $command
+    } else {
+        $functions = Get-Content $file | 
+            Where-Object { $_ -match "function\s" } |
+            ForEach-Object { $_ -replace '^\s*function\s+([^\s\(]+).*$', '$1' }
+
+        # TODO: Detect scripts that aren't dot-sourceable. They can be made dot-sourceable by adding:
+        #           if ($MyInvocation.InvocationName -ne ".") {
+
+        if ($functions.Count -eq 0) {
+            throw "No functions found in $file"
+        }
+
+        $testContent = GetTestContent $functions
     }
 
     New-Item -Path $testFile -ItemType File -Value $testContent | Out-Null
@@ -51,4 +77,3 @@ BeforeAll {
 }
 
 throw "Unrecognized file type: $file"
-
