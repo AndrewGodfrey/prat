392da98222b1e0a4ea150f3419c2ba21afbeaebb
Andrew Godfrey <andrew@gfreys.com>
2025-05-25 19:23:13 -0700

    Test-Prat: Extract script 'Invoke-PesterWithCodeCoverage'

---
+++ pathbin/Invoke-PesterWithCodeCoverage.ps1
@@ -0,0 +1,30 @@
+# .SYNOPSIS
+# Runs Pester with various code-coverage options
+#
+# .PARAMETER CoverageType
+# None:      No code coverage (runs faster)
+# Standard:  Produces CoverageGutter output. Coverage percentage is measured against the entire repo.
+# Subset:    Like Standard, but coverage percentage is measured against the code under the current directory only.
+param (
+    [ValidateSet("None", "Standard", "Subset")] [string] $CoverageType = "Standard",
+    $PathToTest = ".",
+    $RepoRoot = (Resolve-Path "$PSScriptRoot\..")
+)
+
+Import-Module Pester
+$Configuration = [PesterConfiguration]::Default
+$Configuration.Run.Path = $PathToTest
+
+if ($CoverageType -ne "None") {
+    $Configuration.CodeCoverage.OutputPath = "$RepoRoot/auto/coverage.xml"
+    $Configuration.CodeCoverage.Enabled = [bool] $true
+    $Configuration.CodeCoverage.OutputFormat = "CoverageGutters"
+    if ($CoverageType -eq "Subset") {
+        $Configuration.CodeCoverage.Path = $Configuration.Run.Path
+    } else {
+        $Configuration.CodeCoverage.Path = $RepoRoot
+    }
+    $Configuration.CodeCoverage.CoveragePercentTarget = 70
+}
+
+Invoke-Pester -Configuration $Configuration
--- pathbin/Test-Prat.ps1
+++ pathbin/Test-Prat.ps1
@@ -1,31 +1,10 @@
 # .SYNOPSIS
 # Runs tests on Prat
-#
-# .PARAMETER CodeCoverage
-# Enables code coverage measurement
-#
-# .PARAMETER SubdirCodeCoverage
-# A slight tweak on -CodeCoverage - enables measurement and reports
-# coverage percentage just relative to the code in the current directory (instead of all of Prat)
-param ([switch] $CodeCoverage, [switch] $SubdirCodeCoverage)
-
-Import-Module Pester
-$Configuration = [PesterConfiguration]::Default
-$Configuration.Run.Path = "."
-
-if ($SubdirCodeCoverage) { $CodeCoverage = $true }
+param (
+    [ValidateSet("None", "Standard", "Subset")] [string] $CoverageType = "None",
+    [switch] $CodeCoverage)
 
 if ($CodeCoverage) {
-    $pratRoot = Resolve-Path "$PSScriptRoot\.."
-    $Configuration.CodeCoverage.Enabled = [bool] $true
-    $Configuration.CodeCoverage.OutputFormat = "CoverageGutters"
-    if ($SubdirCodeCoverage) {
-        $Configuration.CodeCoverage.Path = $Configuration.Run.Path
-    } else {
-        $Configuration.CodeCoverage.Path = $pratRoot
-    }
-    $Configuration.CodeCoverage.OutputPath = "$pratRoot/auto/coverage.xml"
-    $Configuration.CodeCoverage.CoveragePercentTarget = 70
+    $CoverageType = "Standard"
 }
-
-Invoke-Pester -Configuration $Configuration
+Invoke-PesterWithCodeCoverage -CoverageType $CoverageType -PathToTest "." -RepoRoot (Resolve-Path "$PSScriptRoot\..")
