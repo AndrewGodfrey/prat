d09aa0608f95d7e1ef17c4d1b538f403c1738a70
Andrew Godfrey <andrew@gfreys.com>
2025-05-25 17:11:56 -0700

    Test-Prat: Add switch "-SubdirCodeCoverage"

--- pathbin/Test-Prat.ps1
+++ pathbin/Test-Prat.ps1
@@ -1,14 +1,29 @@
-param ([switch] $CodeCoverage)
+# .SYNOPSIS
+# Runs tests on Prat
+#
+# .PARAMETER CodeCoverage
+# Enables code coverage measurement
+#
+# .PARAMETER SubdirCodeCoverage
+# A slight tweak on -CodeCoverage - enables measurement and reports
+# coverage percentage just relative to the code in the current directory (instead of all of Prat)
+param ([switch] $CodeCoverage, [switch] $SubdirCodeCoverage)
 
 Import-Module Pester
 $Configuration = [PesterConfiguration]::Default
 $Configuration.Run.Path = "."
 
+if ($SubdirCodeCoverage) { $CodeCoverage = $true }
+
 if ($CodeCoverage) {
     $pratRoot = Resolve-Path "$PSScriptRoot\.."
     $Configuration.CodeCoverage.Enabled = [bool] $true
     $Configuration.CodeCoverage.OutputFormat = "CoverageGutters"
-    $Configuration.CodeCoverage.Path = $pratRoot
+    if ($SubdirCodeCoverage) {
+        $Configuration.CodeCoverage.Path = $Configuration.Run.Path
+    } else {
+        $Configuration.CodeCoverage.Path = $pratRoot
+    }
     $Configuration.CodeCoverage.OutputPath = "$pratRoot/auto/coverage.xml"
     $Configuration.CodeCoverage.CoveragePercentTarget = 70
 }
