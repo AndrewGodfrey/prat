617fe17277caf30f3f6a28dc84b58923fa9648b6
Andrew Godfrey <andrew@gfreys.com>
2025-05-25 18:07:51 -0700

    Deploy-Codebase.tests: A random hack to make it work in vscode

--- pathbin/tests/Deploy-Codebase.tests.ps1
+++ pathbin/tests/Deploy-Codebase.tests.ps1
@@ -1,5 +1,10 @@
 BeforeAll {
     . $PSScriptRoot\cbTest.common.ps1
+
+    # I don't understand why this test needs me to edit path, and e.g. Compare-Hash.Tests.ps1 doesn't.
+    # Without this, tests work fine from command line, and in vscode "Debug Test" also works.
+    # But in vscode "Run Test" it fails with "The term 'Deploy-Codebase' is not recognized [...]"
+    $env:path += ";$PSScriptRoot\.."
 }
 
 Describe "Deploy-Codebase" {
