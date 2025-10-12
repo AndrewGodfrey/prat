a9ba72cfe9fdf9e3b95176b2d26ba3742e06eabf
Andrew Godfrey <andrew@gfreys.com>
2025-10-12 14:55:14 -0700

    Build-Prat: Add a note, and tweak the output

--- pathbin/Build-Prat.ps1
+++ pathbin/Build-Prat.ps1
@@ -1,11 +1,10 @@
 # Prat doesn't have a 'build' step per se, but for prat development,
 # it helps to manually unload modules.
 # 
-# Caveat: This doesn't remove classes, like LineArray and InstallationTracker. For iterating on those, the only way I know is to start a new
-#         PS session.
+# Caveat: This doesn't remove classes, like LineArray and InstallationTracker. For iterating on those:
+#         - unit tests: That's solved by Invoke-PesterAsJob
+#         - manual testing: The only way I know, is to move to a new PS session.
 
-function remove($module) {
-    Write-Host -ForegroundColor Green "Removing: $module"
-    Remove-Module $module -ErrorAction SilentlyContinue
-}
-"Installers", "TextFileEditor", "PratBase" | ForEach-Object {remove $_}
+$modules = "Installers", "TextFileEditor", "PratBase"
+Write-Host -ForegroundColor Green "Removing modules: $($modules -join ", ")"
+$modules | ForEach-Object {Remove-Module $_ -ErrorAction SilentlyContinue}
