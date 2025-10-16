2bd85d0f93d482093315365f082dec3fdaa79ba0
Andrew Godfrey <andrew@gfreys.com>
2025-10-15 21:33:56 -0700

    schtasks: Omit 2 files from coverage

--- lib/schtasks/Get-OnLogonScripts_prat.ps1
+++ lib/schtasks/Get-OnLogonScripts_prat.ps1
@@ -1,3 +1,4 @@
 # Gets my list of scripts to run on login
 
 return @()
+# OmitFromCoverageReport: a unit test would just restate it
--- lib/schtasks/daily_test.ps1
+++ lib/schtasks/daily_test.ps1
@@ -1,3 +1,4 @@
 echo "daily_test.ps1 last ran at: $(Get-Date)" | Out-file -encoding UTF8 $home\prat\auto\log\test_schtask.txt
 throw "test failure" # This failure doesn't appear anywhere that I can see.
 
+# OmitFromCoverageReport: a unit test would just restate it
