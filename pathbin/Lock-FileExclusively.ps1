6881ff36d23f768314b5497ad8ae38a692337937
Andrew Godfrey <andrew@gfreys.com>
2025-10-15 20:58:29 -0700

    Lock-FileExclusively.ps1: Add try ... finally

--- pathbin/Lock-FileExclusively.ps1
+++ pathbin/Lock-FileExclusively.ps1
@@ -12,10 +12,11 @@ $FileName = Resolve-Path $FileName
 #Open the file in read only mode, without sharing (I.e., locked as requested)
 $file = [System.io.File]::Open($FileName, 'Open', 'Read', 'None')
 
-#Wait in the above (file locked) state until the user presses a key
-Write-Host "Press any key to continue ..."
-$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
-#Close the file (This releases the current handle and unlocks the file)
-$file.Close()
-
+try {
+    #Wait in the above (file locked) state until the user presses a key
+    Write-Host "Press any key to continue ..."
+    $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+} finally {
+    #Close the file (This releases the current handle and unlocks the file)
+    $file.Close()
+}
