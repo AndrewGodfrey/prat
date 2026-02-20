cefcff8cd5bd6478c69cac1e3b1c140ad2ea7cbe
Andrew Godfrey <andrew@gfreys.com>
2026-02-20 12:57:57 -0800

    ude: Fix regression

--- pathbin/Update-DevEnvironment.ps1
+++ pathbin/Update-DevEnvironment.ps1
@@ -23,7 +23,7 @@ function updateEnvironment($environmentShortcut, [switch] $EnvironmentMayNotExis
         git pull --ff-only
         say "$($environmentShortcut): deploy"
         # Run this in a separate pwsh, for the case when modules have been updated
-        pwsh -NoLogo -Command "Deploy-Codebase"
+        pwsh -NoLogo -Command "Set-Location '$PWD'; Deploy-Codebase"
     } finally {
         Pop-Location
     }
