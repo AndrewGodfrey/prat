23f4928539274dcb7893adf00b84c02372493549
Andrew Godfrey <andrew@gfreys.com>
2026-02-20 22:41:22 -0800

    Invoke-CodebaseCommand: Add coverage

---
+++ lib/Invoke-CodebaseCommand.Tests.ps1
@@ -0,0 +1,28 @@
+BeforeAll {
+    $scriptToTest = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testCbDir = "$PSScriptRoot\..\pathbin\tests\testCb"
+}
+
+Describe "Invoke-CodebaseCommand" {
+    AfterEach { Pop-Location }
+
+    It "Throws when called from an unknown codebase" {
+        New-Item -Type Directory "TestDrive:\unknownCodebase" | Out-Null
+        Push-Location "TestDrive:\unknownCodebase"
+        { &$scriptToTest "build" } | Should -Throw "Unknown codebase*"
+    }
+
+    It "Runs the codebase action script with its env delta applied" {
+        Push-Location $testCbDir
+        $result = &$scriptToTest "test"
+        $result | Should -Be "testCb: test: bar"
+    }
+
+    It "Does nothing when no script is defined for the action" {
+        Push-Location $testCbDir
+        # Temporarily shadow Get-CodebaseScript to return null for this test
+        function Get-CodebaseScript { return $null }
+        $result = &$scriptToTest "build"
+        $result | Should -BeNull
+    }
+}
