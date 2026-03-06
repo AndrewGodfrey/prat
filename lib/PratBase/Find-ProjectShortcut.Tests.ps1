59d8da116f224086185688ffdf4e2e1a5ecc17f7
Andrew Godfrey <andrew@gfreys.com>
2026-03-06 09:17:16 -0800

    `Find-ProjectShortcut` tests: clean up

--- lib/PratBase/Find-ProjectShortcut.Tests.ps1
+++ lib/PratBase/Find-ProjectShortcut.Tests.ps1
@@ -3,54 +3,58 @@ BeforeAll {
 }
 
 Describe "Find-ProjectShortcut" {
-    BeforeEach {
+    BeforeAll {
         $dir = (Get-Item "TestDrive:\").FullName.TrimEnd('\').Replace('\', '/')
         $testProfilePath = "$dir/repoProfile_test.ps1"
+        $simpleRepo = "repoA = @{ root = 'rootA'; subprojects = @{ subA = @{ path = 'subA' } } }"
 
-        "@{ '.' = @{ repos = @{ repoA = @{ root = 'rootA'; subprojects = @{ subA = @{ path = 'subA' } } } }; shortcuts = @{ shortA = 'rootA/foo' } } }" | Out-File $testProfilePath
-        Mock Get-RepoProfileFiles -ModuleName PratBase { return @($testProfilePath) }
+        function makeProfile($repoContent, $shortcutContent, $filename = $testProfilePath) {
+            "@{ '.' = @{ repos = @{ $repoContent }; shortcuts = @{ $shortcutContent } } }" | Out-File $filename
+        }
+    }
+    Context "simple cases" {
+        BeforeAll {
+            makeProfile $simpleRepo "shortA = 'rootA/foo'"
+            Mock Get-RepoProfileFiles -ModuleName PratBase { return @($testProfilePath) }
+        }
+
+        It "Returns the path for a known shortcut" {
+            Find-ProjectShortcut "shortA" | Should -Be "$dir/rootA/foo"
+        }
+
+        It "Returns null when shortcut is not found" {
+            Find-ProjectShortcut "notexist" | Should -BeNull
+        }
+
+        It "Finds subprojects using the full id" {
+            Find-ProjectShortcut "repoA/subA" | Should -Be "$dir/rootA/subA"
+        }
+
+        It "Finds subprojects using the last segment of the id" {
+            Find-ProjectShortcut "subA" | Should -Be "$dir/rootA/subA"
+        }
     }
 
-    It "Returns the path for a known shortcut" {
-        $result = Find-ProjectShortcut "shortA"
-        $result | Should -Be "$dir/rootA/foo"
-    }
+    Context "multiple repo files" {
+        It "Returns all shortcuts as a dict with -ListAll" {
+            $pathB = "$dir/repoProfile_b.ps1"
+            makeProfile $simpleRepo "shortA = 'rootA/foo'"
+            makeProfile "repoB = @{ root = 'rootB' }" "shortB = 'rootB/bar'" $pathB
+            Mock Get-RepoProfileFiles -ModuleName PratBase { return @($testProfilePath, $pathB) }
 
-    It "Returns null when shortcut is not found" {
-        $result = Find-ProjectShortcut "notexist"
-        $result | Should -BeNull
-    }
+            $result = Find-ProjectShortcut -ListAll
 
-    It "Returns all shortcuts as a dict with -ListAll" {
-        $pathB = "$dir/repoProfile_b.ps1"
-        "@{ '.' = @{ repos = @{ repoB = @{ root = 'rootB' } }; shortcuts = @{ shortB = 'rootB/bar' } } }" | Out-File $pathB
-        Mock Get-RepoProfileFiles -ModuleName PratBase { return @($testProfilePath, $pathB) }
+            $result['shortA'] | Should -Be "$dir/rootA/foo"
+            $result['shortB'] | Should -Be "$dir/rootB/bar"
+        }
 
-        $result = Find-ProjectShortcut -ListAll
-        $result['shortA'] | Should -Be "$dir/rootA/foo"
-        $result['shortB'] | Should -Be "$dir/rootB/bar"
-    }
+        It "First file wins when shortcut name appears in multiple files" {
+            $pathB = "$dir/repoProfile_b.ps1"
+            makeProfile "repoA = @{ root = 'a' }" "shared = 'a/from-file1'"
+            makeProfile "repoB = @{ root = 'b' }" "shared = 'b/from-file2'" $pathB
+            Mock Get-RepoProfileFiles -ModuleName PratBase { return @($testProfilePath, $pathB) }
 
-    It "First file wins when shortcut name appears in multiple files" {
-        $pathB = "$dir/repoProfile_b.ps1"
-        "@{ '.' = @{ repos = @{ repoB = @{ root = 'b' } }; shortcuts = @{ shared = 'b/from-file2' } } }" | Out-File $pathB
-
-        $pathA = "$dir/repoProfile_a.ps1"
-        "@{ '.' = @{ repos = @{ repoA = @{ root = 'a' } }; shortcuts = @{ shared = 'a/from-file1' } } }" | Out-File $pathA
-
-        Mock Get-RepoProfileFiles -ModuleName PratBase { return @($pathA, $pathB) }
-
-        $result = Find-ProjectShortcut "shared"
-        $result | Should -Be "$dir/a/from-file1"
-    }
-
-    It "Finds subprojects using the full id" {
-        $result = Find-ProjectShortcut "repoA/subA"
-        $result | Should -Be "$dir/rootA/subA"
-    }
-
-    It "Finds subprojects using the last segment of the id" {
-        $result = Find-ProjectShortcut "subA"
-        $result | Should -Be "$dir/rootA/subA"
+            Find-ProjectShortcut "shared" | Should -Be "$dir/a/from-file1"
+        }
     }
 }
