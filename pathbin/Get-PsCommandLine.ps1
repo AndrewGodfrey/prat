0922d5807f8f9a73f2874c6d5ffb73c038979b44
Andrew Godfrey <andrew@gfreys.com>
2026-07-13 22:51:48 -0700

    Get-PsCommandLine: fix

--- pathbin/Get-PsCommandLine.ps1
+++ pathbin/Get-PsCommandLine.ps1
@@ -2,6 +2,6 @@ param ([string] $nameMatch)
 return ,@(
     Get-CimInstance Win32_Process | 
     ? { $_.Name -like $nameMatch } | 
-    % { @{ Id=$_.ProcessId; ProcessName=$_.ProcessName; CommandLine=$_.CommandLine } }
+    % { [pscustomobject]@{ Id=$_.ProcessId; ProcessName=$_.Name; CommandLine=$_.CommandLine } }
     )
 # OmitFromCoverageReport: a unit test would just restate it - this is a wrapper for Windows-specific crud
